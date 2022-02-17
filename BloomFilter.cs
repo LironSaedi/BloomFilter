2712e621d6a30cdb2479cf1c69d3e6384f4ad66c
Liron Saedi <Liron.Saedi@GMR.local>
2022-02-16 18:19:02 -0800

    got some good progreess on le bloom filter

---
+++ BloomFilter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloomFilter
+{
+    class BloomFilter<T>
+    {
+        bool[] array;
+        Func<T, int>[] hashFunctions;
+        public BloomFilter(int cap, Func<T, int>[] hashFunctions)
+        {
+            this.array = new bool[cap];
+            this.hashFunctions = hashFunctions;
+        }
+
+        public void Insert(T item)
+        {
+            int hashCode = Math.Abs(HashFuncOne(item));
+            hashCode = hashCode % array.Length;
+            array[hashCode] = true;
+        }
+
+        public bool ProbablyContains(T item)
+        {
+            int hashCode = Math.Abs(HashFuncOne(item));
+            hashCode = hashCode % array.Length;
+
+            if (!(array[hashCode]))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private int HashFuncOne(T item)
+        {
+            //Tom should FIXME
+            return 1000;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BloomFilter
+{
+    class Program
+    {
+        static int AddOne(int n)
+        {
+            return n + 1;
+        }
+
+        static void Main(string[] args)
+        {
+            Func<string, int> hash1 = (string s) => s.GetHashCode();
+            Func<string, int> hash2 = (string s) => (s + "adsafs").GetHashCode();
+
+            Func<int, int> add = AddOne;
+            int y = add(23);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
